1cb0bf6b5b370269a5f43019f076b16e1e684db1
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2024-10-14 16:53:40 -0300

    Java Spring Boot - 6 - Funcionamento da Rota - Erro

--- AppRH/src/main/java/com/AppRH/AppRH/controllers/FuncionarioController.java
+++ AppRH/src/main/java/com/AppRH/AppRH/controllers/FuncionarioController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.AppRH.AppRH.models.Funcionario;
@@ -44,4 +45,5 @@ public class FuncionarioController {
 		return "redirect:/cadastrarFuncionario";
 
 	}
+
 }
--- AppRH/src/main/java/com/AppRH/AppRH/repository/DependentesRepository.java
+++ AppRH/src/main/java/com/AppRH/AppRH/repository/DependentesRepository.java
@@ -1,5 +1,22 @@
 package com.AppRH.AppRH.repository;
 
-public interface DependentesRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.AppRH.AppRH.models.Dependentes;
+import com.AppRH.AppRH.models.Funcionario;
+
+public interface DependentesRepository extends CrudRepository<Dependentes, Long>{
+
+	Iterable<Dependentes> findByFuncionario(Funcionario funcionario);
+	
+	
+	//PENSANDO NO MÉTODO DELETE
+	Dependentes findByCpf(String cpf);
+	Dependentes findById(long id);
+	
+	//CRIADO PARA IMPLEMENTAR 
+	List<Dependentes> findByNome(String nome);
+	
 }
