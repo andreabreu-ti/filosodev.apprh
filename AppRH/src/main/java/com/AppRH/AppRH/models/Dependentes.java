1315fe763a91ad785982cba0220dad92b6a7842d
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2024-10-14 14:23:08 -0300

    Java Spring Boot - 4 - Criando as entidades Funcionario e Dependente

---
+++ AppRH/src/main/java/com/AppRH/AppRH/models/Dependentes.java
@@ -0,0 +1,62 @@
+package com.AppRH.AppRH.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Dependentes implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(unique = true)
+	private String cpf;
+
+	private String nome;
+	private String dataNascimento;
+
+	@ManyToOne
+	private Funcionario funcionario;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+}
